764d5afe129082b968dd8b48249357f12ffa65d6
AKKELD <t9107115@gmail.com>
2026-09-13 15:16:56 +0900

    integertriangle sol
    컴파일 오류없음
    런타임 에러 없음
    원하는 값 출력 완료
    
    효율성 테스트 실패

--- justplay/src/programmers/practice/integertriangle/Solution.java
+++ justplay/src/programmers/practice/integertriangle/Solution.java
@@ -23,19 +23,18 @@ class Solution {
         smalltriangle = new int[tri_maxlength-1][];
         for (int row = 0; row < tri_maxlength-1; row++)
             smalltriangle[row] = new int[triangle[row].length];
-
-//        for (int i=0; i<tri_maxlength-1; i++){
-//            if(triangle[tri_maxlength][i]>triangle[tri_maxlength][i+1])
-//                smalltriangle[tri_maxlength-1][i] = triangle[tri_maxlength][i]+triangle[tri_maxlength-1][i];
-//            else
-//                smalltriangle[tri_maxlength-1][i] = triangle[tri_maxlength][i+1]+triangle[tri_maxlength-1][i];
-//        }
-        for (int row=0; row<tri_maxlength-1; row++){
-            for (int col=0; col<triangle[row].length; col++){
-                if (triangle[row+1][col]>=triangle[row+1][col+1]) smalltriangle[row][col]=triangle[row+1][col]+triangle[row][col];
-                else smalltriangle[row][col]=triangle[row+1][col+1]+triangle[row][col];
-            }
+        for (int row=0; row<tri_maxlength-2; row++)
+            smalltriangle[row] = triangle[row];
+        for (int col=0; col<triangle[tri_maxlength-2].length; col++){
+            smalltriangle[tri_maxlength-2][col] = triangle[tri_maxlength-2][col] + Math.max(triangle[tri_maxlength-1][col], triangle[tri_maxlength-1][col+1]);
         }
+//        for (int row=0; row<tri_maxlength-1; row++){
+//            for (int col=0; col<triangle[row].length; col++){
+//                if (triangle[row+1][col]>=triangle[row+1][col+1]) smalltriangle[row][col]=triangle[row+1][col]+triangle[row][col];
+//                else smalltriangle[row][col]=triangle[row+1][col+1]+triangle[row][col];
+//            }
+//        }
+// "자기 행 + 바로 아래 행"의 값이 더해져 있는데, 다음 재귀 호출에서 또 같은 방식으로 (지금의 행) + (지금의 아래 행)을 더합니다. 즉 중간에 있던 행의 값이 재귀가 반복될수록 여러 번 중복으로 누적
         if (smalltriangle.length != 1) toSmallTriangle(smalltriangle);
         return smalltriangle;
     }
