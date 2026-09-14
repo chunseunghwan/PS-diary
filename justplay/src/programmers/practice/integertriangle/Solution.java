aeee933eb6ddc196c1136fc9fb865bcce7dc09ef
AKKELD <t9107115@gmail.com>
2026-09-14 12:37:48 +0900

    fix

--- justplay/src/programmers/practice/integertriangle/Solution.java
+++ justplay/src/programmers/practice/integertriangle/Solution.java
@@ -12,31 +12,46 @@ class Solution {
 
     public int solution(int[][] triangle) {
         int answer = 0;
+        int n = triangle.length;
+        int[][] addMaxArray = new int[n][n];
 
-        answer = toSmallTriangle(triangle)[0][0];
+
+        for (int i=0; i<n ; i++){
+            for(int j=0; j<triangle[i].length; j++){
+                if (i==0 && j==0) {
+                    addMaxArray[i][j] = triangle[i][j];
+                    continue;
+                }
+
+                int bringUpperLeft = (j != 0) ? addMaxArray[i-1][j-1] : 0;
+                int bringUpperRight = addMaxArray[i-1][j];
+
+                addMaxArray[i][j] = triangle[i][j] + Math.max(bringUpperLeft, bringUpperRight);
+            }
+        }
+        int max = 0;
+        for (int i : addMaxArray[n-1]){
+            max = Math.max(i, max);
+        }
+
+        answer = max;
+        //answer = toSmallTriangle(triangle)[0][0];
         return answer;
     }
 
-    int[][] toSmallTriangle(int[][] triangle){
-
-        int tri_maxlength = triangle.length; //5
-        smalltriangle = new int[tri_maxlength-1][];
-        for (int row = 0; row < tri_maxlength-1; row++)
-            smalltriangle[row] = new int[triangle[row].length];
-        for (int row=0; row<tri_maxlength-2; row++)
-            smalltriangle[row] = triangle[row];
-        for (int col=0; col<triangle[tri_maxlength-2].length; col++){
-            smalltriangle[tri_maxlength-2][col] = triangle[tri_maxlength-2][col] + Math.max(triangle[tri_maxlength-1][col], triangle[tri_maxlength-1][col+1]);
-        }
-//        for (int row=0; row<tri_maxlength-1; row++){
-//            for (int col=0; col<triangle[row].length; col++){
-//                if (triangle[row+1][col]>=triangle[row+1][col+1]) smalltriangle[row][col]=triangle[row+1][col]+triangle[row][col];
-//                else smalltriangle[row][col]=triangle[row+1][col+1]+triangle[row][col];
-//            }
+//    int[][] toSmallTriangle(int[][] triangle){
+//
+//        int tri_maxlength = triangle.length;
+//        smalltriangle = new int[tri_maxlength-1][];
+//        for (int row = 0; row < tri_maxlength-1; row++)
+//            smalltriangle[row] = new int[triangle[row].length];
+//        for (int row=0; row<tri_maxlength-2; row++)
+//            smalltriangle[row] = triangle[row];
+//        for (int col=0; col<triangle[tri_maxlength-2].length; col++){
+//            smalltriangle[tri_maxlength-2][col] = triangle[tri_maxlength-2][col] + Math.max(triangle[tri_maxlength-1][col], triangle[tri_maxlength-1][col+1]);
 //        }
-// "자기 행 + 바로 아래 행"의 값이 더해져 있는데, 다음 재귀 호출에서 또 같은 방식으로 (지금의 행) + (지금의 아래 행)을 더합니다. 즉 중간에 있던 행의 값이 재귀가 반복될수록 여러 번 중복으로 누적
-        if (smalltriangle.length != 1) toSmallTriangle(smalltriangle);
-        return smalltriangle;
-    }
+//        if (smalltriangle.length != 1) toSmallTriangle(smalltriangle);
+//        return smalltriangle;
+//    }
 }
 
